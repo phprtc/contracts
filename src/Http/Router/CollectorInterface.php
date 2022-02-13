515c444c17ecec51d3ed9eae94dd7e978244b44a
ahmard <ahmard06@gmail.com>
2022-02-13 19:01:55 +0100

    add more route collector methods

--- src/Http/Router/CollectorInterface.php
+++ src/Http/Router/CollectorInterface.php
@@ -15,4 +15,16 @@ interface CollectorInterface
     public function register(): static;
 
     public function getFastRouteCollector(bool $createNew = false): \FastRoute\RouteCollector;
+
+    public function getCollectedRoutes(): array;
+
+    public function getCachedRoutes(): array;
+
+    public function getFastRouteData(): array;
+
+    public function isRegistered(): bool;
+
+    public function route(string $routeName): RouteDataInterface|null;
+
+    public function uri(string $routeName, array $routeParams = []): string|null;
 }
