c115cf64a12dede9925585824e71e56536e6a9e8
ahmard <ahmard06@gmail.com>
2022-02-13 22:01:35 +0100

    added error response generator

--- src/Http/RequestInterface.php
+++ src/Http/RequestInterface.php
@@ -13,4 +13,13 @@ interface RequestInterface extends \Psr\Http\Message\RequestInterface
     public function getMiddleware(): RequestMiddlewareInterface;
 
     public function getRouteDispatchResult(): DispatchResultInterface;
+
+    /**
+     * This method will send error response to client
+     * this should be used in try-catch statement
+     *
+     * @param callable $callback
+     * @return void
+     */
+    public function handleException(callable $callback): void;
 }
