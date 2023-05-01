9e2c837dad39ac7a989c6eeeda277d3a195576ca
ahmard <ahmard.mu@gmail.com>
2023-05-01 02:20:31 +0100

    feat(ws): room terms

---
+++ src/Enums/WSRoomTerm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RTC\Contracts\Enums;
+
+enum WSRoomTerm: string
+{
+    case JOIN = 'join';
+    case JOINED = 'joined';
+    case MESSAGE = 'message';
+    case LEAVE = 'leave';
+    case LEFT = 'left';
+    case REMOVE = 'remove';
+    case CREATE = 'create';
+
+
+    case USER_JOINED = 'user_joined';
+    case USER_LEFT = 'user_left';
+}
