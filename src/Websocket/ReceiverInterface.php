ede6bba5bdcff889eb181e021564e5e101fd6013
ahmard <ahmard.mu@gmail.com>
2023-05-01 22:22:42 +0100

    feat: ability to check if receiver is valid

--- src/Websocket/ReceiverInterface.php
+++ src/Websocket/ReceiverInterface.php
@@ -5,5 +5,8 @@ namespace RTC\Contracts\Websocket;
 interface ReceiverInterface
 {
     public function getType(): string;
+
     public function getId(): string;
+
+    public function isValid(): bool;
 }
